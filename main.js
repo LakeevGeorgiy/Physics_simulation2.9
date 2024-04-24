dfd9d69f5f98afc763e1359fb7363f00646758fb
LakeevGeorgiy <lakeev.gosha2016@yandex.ru>
2024-04-24 14:35:04 +0300

    1.1.0

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 let input_frequency_text = document.getElementById("input_frequency_id");
 let information_frequency_text = document.getElementById("information_frequency_id");
-let amplitude_text = document.getElementById("amplitude_id");
+let input_amplitude_text = document.getElementById("input_amplitude_id");
+let information_amplitude_text = document.getElementById("information_amplitude_id")
 let button = document.getElementById("button_id");
 
 let input_frequency = input_frequency_text.value;
 let information_frequency = information_frequency_text.value;
-let amplitude = amplitude_text.value;
+let input_amplitude = input_amplitude_text.value;
+let information_amplitude = information_amplitude_text.value;
+let m = information_amplitude / input_amplitude;
 
 function calculateCarrierWave(t){
-	return amplitude * Math.cos(input_frequency * t);
+	return input_amplitude * Math.cos(input_frequency * t);
 }
 
 function calculateInformationWave(t){
-    return amplitude * Math.cos(information_frequency * t);
+    return information_amplitude * Math.cos(information_frequency * t);
 }
 
 function calculateAmplitudeModulation(t){
-    return amplitude * (1 + Math.cos(information_frequency * t)) * Math.cos(input_frequency * t);
+    return input_amplitude * (1 + m * Math.cos(information_frequency * t)) * Math.cos(input_frequency * t);
 }
 
 function calculateSpectrum(t){
@@ -33,21 +36,25 @@ function plot() {
 	let informationWave = [];
 	let amplitudeModulation = [];
     let spectrum = [];
+
+	let f0 = input_frequency / (2 * Math.PI);
+	let F = information_frequency / (2 * Math.PI);
     
-    let xSpectrumInput = [-input_frequency, input_frequency];
-    let spectrumInput = [amplitude, amplitude];
+    let xSpectrumInput = [-f0, f0];
+    let spectrumInput = [input_amplitude, input_amplitude];
 
-    let xSpectrumInformation = [-information_frequency, information_frequency];
-    let spectrumInformation = [amplitude, amplitude];
+    let xSpectrumInformation = [-F, F];
+    let spectrumInformation = [information_amplitude, information_amplitude];
 
-    let xSpectrumModulation = [-input_frequency - information_frequency, -input_frequency, -input_frequency + information_frequency,
-        input_frequency - information_frequency, input_frequency, input_frequency + information_frequency];
-    let spectrumModulation = [amplitude /2, amplitude, amplitude / 2, amplitude / 2, amplitude, amplitude / 2];
+    let xSpectrumModulation = [-f0 - F, -f0, -f0 + F, f0 - F, f0, f0 + F];
+    let spectrumModulation = [m * input_amplitude /2, input_amplitude, m * input_amplitude / 2, 
+							m * input_amplitude / 2, input_amplitude, m * input_amplitude / 2];
 
 
 
     let T = 2 * Math.PI / information_frequency;
-    for (let t = 0; t < 5 * T; t += 0.001) {
+	let step = 5 * T / 10000;
+    for (let t = 0; t < 5 * T; t += step) {
 
         time_coordinates.push(t);
         carrierWave.push(calculateCarrierWave(t));
@@ -131,10 +138,10 @@ function plot() {
 		}
 	};
 
-    let widthPlot = 1.5 * input_frequency + 2 * information_frequency;
+    let widthPlot = 1.5 * F + 2 * f0;
 
     let layout4 = {
-        title: 'Спектр несущего сигнала',
+        title: 'Спектр несущего сигнала',	
 	    autosize: true,
 		xaxis: {
 			title: 'Omega, Гц',
@@ -180,23 +187,40 @@ function plot() {
 button.addEventListener("click", function(e){
 
 	input_frequency = parseFloat(input_frequency_text.value);
-	if (input_frequency < 0) {
-		alert("Частота меньше 0!");
+	if (input_frequency <= 0) {
+		alert("Частота несущего сигнала должна быть больше 0!");
 		return;
 	}
 	
 	information_frequency = parseFloat(information_frequency_text.value);
-	if (information_frequency < 0) {
-		alert("Частота меньше 0!");
+	if (information_frequency <= 0) {
+		alert("Частота информационного сигнала должна быть больше 0!");
 		return;
 	}
 
-	amplitude = parseFloat(amplitude_text.value);
-	if (amplitude < 0) {
+
+	if (information_frequency > input_frequency){
+		alert("Частота информационного сигнала должна быть меньше частоты несущего сигнала!");
+		return;
+	}
+
+	// if (information_frequency * 10 > input_frequency){
+	// 	alert("Возможно частота несущего сигнала должна быть больше для корректной работы!");
+	// }
+
+	input_amplitude = parseFloat(input_amplitude_text.value);
+	if (input_amplitude < 0) {
+		alert("Амплитуда меньше 0!");
+		return;
+	}
+
+	information_amplitude = parseFloat(information_amplitude_text.value);
+	if (information_amplitude < 0) {
 		alert("Амплитуда меньше 0!");
 		return;
 	}
 	
+	m = information_amplitude / input_amplitude;
     plot();
 
 });
